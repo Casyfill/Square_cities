47acefa8ce39b2029573396439062ad622129f38
casyfill <casyfill@gmail.com>
2015-11-02 10:38:19 -0500

    credentials connected

--- scraper/4sqr_code_v46.py
+++ scraper/4sqr_code_v46.py
@@ -1,130 +1,157 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#  два режима (сервер и локал) с подстановкой путей
-mode = 's' #'l'
+############# TODOS
+# [X] credentials
+# [ ] paths
+# [ ] extract place_research
+# [ ] overal architecture
+# [ ] 
 
-pathes = {'l':{'modulePath':"/Users/casy/Dropbox (RN&IA'N)/Projects/Kats/Afisha/4sqr_scrape/code/misc/",
-					'resultPath':"/Users/casy/Dropbox (RN&IA'N)/Projects/Kats/Afisha/4sqr_scrape/data/"},
-		  's':{'modulePath':"/root/4sqr_scraper/code/misc",
-					'resultPath':"/root/4sqr_scraper/data/"}}
+#  two modes: server (s) and local (l)
+mode = 's'  # 'l'
 
-modulePath, resultPath = pathes[mode]['modulePath'],pathes[mode]['resultPath']
+
+#TODO: need to rewrite that
+pathes = {'l': {'modulePath': "/Users/casy/Dropbox (RN&IA'N)/Projects/Kats/Afisha/4sqr_scrape/code/misc/",
+                'resultPath': "/Users/casy/Dropbox (RN&IA'N)/Projects/Kats/Afisha/4sqr_scrape/data/"},
+          's': {'modulePath': "/root/4sqr_scraper/code/misc",
+                'resultPath': "/root/4sqr_scraper/data/"}}
+
+modulePath, resultPath = pathes[mode]['modulePath'], pathes[mode]['resultPath']
 
 # import requests,time,json
-import sys, csv, time
-sys.path.append(modulePath)
+import sys
+import csv
+import time
+import json
+import os
+# sys.path.append(modulePath)
+SQUARE = os.genenv('SQUARE')
 
-from frsqr_path import CLIENT_SECRET, CLIENT_ID
+with open(SQUARE + 'scraper/credentials.json') as data_file:    
+    credentials = json.load(data_file)
+
+CLIENT_SECRET = credentials['data']['CLIENT_SECRET']
+CLIENT_ID     = credentials['data']['CLIENT_ID']
+
+
 from matrix import matrix
 from boundChecker import checkVsBound, checkArrayVsBound
 
-import parseVenue, frsqrRequests
-# from graphics import *
-# win = GraphWin()
-
+import parseVenue
+import frsqrRequests
 
 
 sleepTime = 20
 place = 'Oklahoma3'
 swBound = '33.329980, -103.567494'
 neBound = '37.384914, -94.111012'
-resultPath+=(place + '.csv')
+resultPath += (place + '.csv')
 
 
-headers=['genCategory',   # csv headers (params)
-				'category',
-				'name',
-				'lon',
-				'lat',
-				'checkIns',
-				'tips',
-				'users',
-				'createdAt',
-				'tileID',
-				'ID',
-				'place',
-				'time',
-				'verified',
-	 			'price',
-	 			'rating',
-	 			'tags',
-	 			'photoCount',
-	 			'description']
+headers = ['genCategory',   # csv headers (params)
+           'category',
+           'name',
+           'lon',
+           'lat',
+           'checkIns',
+           'tips',
+           'users',
+           'createdAt',
+           'tileID',
+           'ID',
+           'place',
+           'time',
+           'verified',
+           'price',
+           'rating',
+           'tags',
+           'photoCount',
+           'description']
 
 
 with open(resultPath, 'w') as csvfile:
-	writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	writer.writerow(headers)
+    writer = csv.writer(csvfile, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    writer.writerow(headers)
 
-	newTileArray  = [{'sw':swBound, 'ne':neBound,'name':'0'}] #First list of requests
-	catArrays = frsqrRequests.generateCatArray(CLIENT_SECRET, CLIENT_ID) #list of categories
-	
-	level = 0 #уровень детализации 
-	spreads = 1 # количество необходимых запростов
-	read = 0 # количество результативных запросов
+    # First list of requests
+    newTileArray = [{'sw': swBound, 'ne': neBound, 'name': '0'}]
+    catArrays = frsqrRequests.generateCatArray(
+        CLIENT_SECRET, CLIENT_ID)  # list of categories
 
-	totalVenues = 0 # счетчик собранных мест
+    level = 0  # уровень детализации
+    spreads = 1  # количество необходимых запростов
+    read = 0  # количество результативных запросов
 
-	while len(newTileArray)>0:
-		
-		tileArray = newTileArray # рабочий список
-		newTileArray = [] #обнуляем список для следующей итерации
-		level+=1
-		print 'level %d reached: %d requests, %d venues so far!' %(level, len(tileArray), totalVenues)
+    totalVenues = 0  # счетчик собранных мест
 
-		for tile in tileArray:
-			
-			try:
-				ask = frsqrRequests.VenueSearch(tile['sw'],tile['ne'],CLIENT_ID,CLIENT_SECRET)
+    while len(newTileArray) > 0:
 
-				
-				# проверка статусов
-				if ask['meta']['code']!=200: #если ответ не нормальный
+        tileArray = newTileArray  # рабочий список
+        newTileArray = []  # обнуляем список для следующей итерации
+        level += 1
+        print 'level %d reached: %d requests, %d venues so far!' % (level, len(tileArray), totalVenues)
 
-					if ask['meta']['errorType']=='geocode_too_big':
-						newTileArray+=matrix(tile['sw'],tile['ne'],tile['name']) # добавляем детализированную матрицу к листу следующего уровня
-						print tile['name'], ':', read, '/',spreads, ' too big, detailed!'
-						spreads+=3
-					
-					# ПОЧЕМУ-ТО ДО ЭТОГО МЕСТА НЕ ДОХОДИТ :-(((
-					else:
-						print tile['name'], ':', ask['meta']['errorType']
-						newTileArray.append(tile)
+        for tile in tileArray:
 
-				else:
+            try:
+                ask = frsqrRequests.VenueSearch(
+                    tile['sw'], tile['ne'], CLIENT_ID, CLIENT_SECRET)
 
-					p = checkArrayVsBound(tile['sw'],tile['ne'],ask[u"response"][u"venues"])
-					
-					if len(p)>=50:  #если запросов 50, то, скорее всего, включен лимит, нужна детализация
-						newTileArray+=matrix(tile['sw'],tile['ne'],tile['name']) # добавляем детализированную матрицу к листу следующего уровня
-						print tile['name'], ':', read, '/',spreads, ' detailed!'
-						spreads+=3
-						
-					elif len(p)==0: #если ответ путой
-						read+=1
-						print tile['name'], ':', read, '/',spreads, 'zone empty'
-					else: #если ответ не пустой
-						read+=1
-						print tile['name'], ':', read, '/',spreads, 'saved: %d venues' % (len(p))
+                # проверка статусов
+                if ask['meta']['code'] != 200:  # если ответ не нормальный
 
-						for venue in p:
-							ID = venue['id']
-							# Получаю полные данные по Venue
-							full_venue = frsqrRequests.getCompleteDetails(ID,CLIENT_ID,CLIENT_SECRET, sleepTime)
-							v = parseVenue.parseVenue(full_venue, catArrays, tile['name'], place)
-							writer.writerow([v[key] for key in headers])
-						
-						totalVenues+=len(p)
-						
+                    if ask['meta']['errorType'] == 'geocode_too_big':
+                        # добавляем детализированную матрицу к листу следующего
+                        # уровня
+                        newTileArray += matrix(tile['sw'],
+                                               tile['ne'], tile['name'])
+                        print tile['name'], ':', read, '/', spreads, ' too big, detailed!'
+                        spreads += 3
 
-				time.sleep(sleepTime)
-			except Exception,e: 
-				print str(e)
-				print tile['name'], ':', read, '/',spreads, 'error while asking!'
-				newTileArray.append(tile)
-				time.sleep(sleepTime*5)
+                    # ПОЧЕМУ-ТО ДО ЭТОГО МЕСТА НЕ ДОХОДИТ :-(((
+                    else:
+                        print tile['name'], ':', ask['meta']['errorType']
+                        newTileArray.append(tile)
 
+                else:
 
-	print 'scraping %s done!, venues: %d' % (place, totalVenues)
+                    p = checkArrayVsBound(tile['sw'], tile['ne'], ask[
+                                          u"response"][u"venues"])
 
+                    if len(p) >= 50:  # если запросов 50, то, скорее всего, включен лимит, нужна детализация
+                        # добавляем детализированную матрицу к листу следующего
+                        # уровня
+                        newTileArray += matrix(tile['sw'],
+                                               tile['ne'], tile['name'])
+                        print tile['name'], ':', read, '/', spreads, ' detailed!'
+                        spreads += 3
+
+                    elif len(p) == 0:  # если ответ путой
+                        read += 1
+                        print tile['name'], ':', read, '/', spreads, 'zone empty'
+                    else:  # если ответ не пустой
+                        read += 1
+                        print tile['name'], ':', read, '/', spreads, 'saved: %d venues' % (len(p))
+
+                        for venue in p:
+                            ID = venue['id']
+                            # Получаю полные данные по Venue
+                            full_venue = frsqrRequests.getCompleteDetails(
+                                ID, CLIENT_ID, CLIENT_SECRET, sleepTime)
+                            v = parseVenue.parseVenue(
+                                full_venue, catArrays, tile['name'], place)
+                            writer.writerow([v[key] for key in headers])
+
+                        totalVenues += len(p)
+
+                time.sleep(sleepTime)
+            except Exception, e:
+                print str(e)
+                print tile['name'], ':', read, '/', spreads, 'error while asking!'
+                newTileArray.append(tile)
+                time.sleep(sleepTime * 5)
+
+    print 'scraping %s done!, venues: %d' % (place, totalVenues)
--- scraper/README.md
+++ scraper/README.md
@@ -1,6 +1,15 @@
 notes_on_scraping
 =================
 
+## TODO
+- [X] credentials
+- [ ] update paths
+- [ ] extract place_data
+- [ ] overal architecture
+- [ ] make scraper bash-workable
+- [ ] additional search_checker
+
+
 Here will be a small instruction on data scraping.
 
 1. As we need a lot of data, lets EACH create a 4sqr developer account here: [4square dev](https://developer.foursquare.com/)
@@ -8,3 +17,26 @@ Here will be a small instruction on data scraping.
 2. I am going to refactor my scraper in one or two days, as it has some flaws (sometimes 4square returns ~4 venues in the dence areas, so I need to add re-checker on those suspecious returns. Also, I will add support for command-line start, remove all exposed pathes and some minor fixtures.
 
 3. Please, let's agree that each of us will keep his credentials in the **credentials.json** file: it is a template for now, I will add it to .gitignore after this commit. Let's also agree on using **SQUARE** alias for our pathes, so **SQUARE** will be root folder of this repository. Path to credentials will be SQUARE + 'scraper/credentials.json' this way.
+
+## Attributes (scraping now)
+
+- genCategory (one of 8 general categoris) 
+- category (one of ~290 categories)
+- name
+- lon
+- lat
+- checkIns total
+- tips counter
+- (unique) users total
+- createdAt (creation date)
+- tileID (I use tile query approach)
+- ID (unique venue ID)
+- place (?)
+- time (?)
+- verified (shows is venue account is managed by the place owner)
+- price (average prices, if applicable)
+- rating (average rating of the place)
+- tags (place tag cloud)
+- photoCount (number of photos for venue)
+- description (textual place description)
+
